555e0c5d88b9b2f9cb39a80d554f8082605b7ed8
mtanner161 <m.tanner161@gmail.com>
2026-04-02 20:06:01 -0500

    afesummary to offset wells

--- main_model.py
+++ main_model.py
@@ -8,4 +8,14 @@ import shalehavenscripts.production as production
 
 token = novi.authNovi()
 
-print("Successfully Authenticated with Novi. Token:", token)
+print("Successfully Authenticated with Novi Token:")
+
+pathToAfeSummary = "C:\\Users\\MichaelTanner\\OneDrive - Shalehaven Energy & Asset Management LLC\\A&D - Documents\\# 2026 LP\\Bone Springs - Eddy County Mewbourne\\AFE Summary - Milkshake.xlsx"
+
+# Read in AFE Summary
+
+afeData = novi.readAFESummary(pathToAfeSummary)
+
+offsetData = novi.getWells(token, afeData)
+
+x = 5
--- shalehavenscripts/novi.py
+++ shalehavenscripts/novi.py
@@ -3,9 +3,18 @@
 
 import requests
 import os
+import pandas as pd
 
 
 BASE_URL = "https://insight.novilabs.com/api/"
+
+## Read in AFE Summary from Excel file
+def readAFESummary(pathToFile):
+    
+    afeData = pd.read_excel(pathToFile)
+    
+    return afeData
+
 
 ## Novi API Authentication and Data Retrieval Functions
 def authNovi():
@@ -15,13 +24,55 @@ def authNovi():
     session = requests.Session()
 
     response = session.post(BASE_URL + "v2/sessions", json={"email": username, "password": password})
+    
     response.raise_for_status()
 
     token = response.json()["authentication_token"]
 
     return token
 
-def getWells(token):
-   x= 5
+
+# Function to retrieve wells based on AFE Summary data
+def getWells(token, afeData, scope="us-horizontals"):
    
-   return x
+   # pull out Township, Range, County, State, and Landing Zone from AFE Summary data
+    township = afeData["Township"].iloc[0]
+    range = afeData["Range"].iloc[0]
+    county = afeData["County"].iloc[0]
+    state = afeData["State"].iloc[0]
+    formation = afeData["Landing Zone"].iloc[0]
+
+    params = {
+        "authentication_token": token,
+        "scope": scope,
+        "q[Township_eq]": township,
+        "q[Range_eq]": range,
+        "q[County_eq]": county,
+        "q[State_eq]": state,
+        "q[Formation_eq]": formation,
+    }
+
+    all_wells = []
+    page = 1
+
+    while True:
+        params["page"] = page
+        response = requests.get(BASE_URL + "v3/wells.json", params=params)
+        response.raise_for_status()
+
+        data = response.json()
+
+        if not data:
+            break
+
+        all_wells.extend(data)
+
+        # If fewer results than a full page, we've reached the end
+        if len(data) < 100:
+            break
+
+        page += 1
+
+    return pd.DataFrame(all_wells)
+
+
